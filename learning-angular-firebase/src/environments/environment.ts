28fc7785506d505fb6c7fe27c3fb43abdb6e505b
HARSHVARDHANSINHA <harshvsinha18@.com>
2023-06-06 17:23:36 +0530

    Firebase Initiall Setup Done

---
+++ learning-angular-firebase/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyC9TmV5X1rDBoF8gWiwWd02dosCZLzKEhU",
+  authDomain: "learning-angular-firebas-d5d91.firebaseapp.com",
+  projectId: "learning-angular-firebas-d5d91",
+  storageBucket: "learning-angular-firebas-d5d91.appspot.com",
+  messagingSenderId: "250498409131",
+  appId: "1:250498409131:web:2fe21faf56f7e582694dee"
+},
+  };
